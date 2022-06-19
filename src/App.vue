41bc3d725fba521c23e21ff45d8340166e0298f2
benma77777 <2533676796@qq.com>
2022-06-19 12:37:36 +0800

    完成点击切换顺序

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-
+    <ul>
+      <li v-for="item in arr" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+    <button @click="btn">走一走</button>
   </div>
 </template>
 
@@ -8,11 +13,18 @@
 export default {
   data() {
     return {
-     
+      arr: ["程序员", "帅哥", "美女"],
     };
+  },
+  methods: {
+    btn() {
+      // this.arr.push(this.arr[0]);
+      // this.arr.shift()
+
+      this.arr.push(this.arr.shift());
+    },
   },
 };
 </script>
-
 <style>
 </style>
